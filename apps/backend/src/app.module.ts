5b49d60c99d3a81ba4d9490cdfb035a212d6342b
Sámuel Fekete <feketesamu@gmail.com>
2024-05-13 20:51:15 +0200

    user resource

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
+  imports: [PrismaModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
