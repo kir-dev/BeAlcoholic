aff16b072b33abfa3449673e85642dfaf5e8de2f
Farkasházi Levente <levente.farkashazi@zooshdigital.com>
2024-05-15 23:24:11 +0200

    commented out parameters in controllers

--- apps/backend/src/events/events.controller.ts
+++ apps/backend/src/events/events.controller.ts
@@ -1,8 +1,5 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
-import { Event } from './entities/event.entity';
 import { EventsService } from './events.service';
 
 @Controller('events')
@@ -10,27 +7,27 @@ export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  create(@Body() data: CreateEventDto): Promise<Event> {
-    return this.eventsService.create(data);
+  async create(/*@Body() data: CreateEventDto*/) {
+    return this.eventsService.create(/*data*/);
   }
 
   @Get()
-  findAll(): Promise<Event[]> {
+  findAll() {
     return this.eventsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseUUIDPipe()) id: string): Promise<Event> {
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.eventsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id', new ParseUUIDPipe()) id: string, @Body() data: UpdateEventDto): Promise<Event> {
-    return this.eventsService.update(id, data);
+  update(@Param('id', new ParseUUIDPipe()) id: string /*, @Body() data: UpdateEventDto*/) {
+    return this.eventsService.update(id /*,data*/);
   }
 
   @Delete(':id')
-  remove(@Param('id', new ParseUUIDPipe()) id: string): Promise<Event> {
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.eventsService.remove(id);
   }
 }
