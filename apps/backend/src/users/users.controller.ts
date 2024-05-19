333fbca7abc7ec6e074f3e4b0f857fc2231b740e
Farkasházi Levente <farkashazilevente2@gmail.com>
2024-05-19 22:30:42 +0200

    feat: Add @ApiTags('Users') to UsersController

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
