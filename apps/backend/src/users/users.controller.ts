1077c68cf98f83964ecc2dd9f892d2db61ef189f
Farkasházi Levente <79463263+LeventeFarkashazi@users.noreply.github.com>
2024-05-21 21:08:01 +0200

    12 create endpoints for setting the favourite drinks of a user (#14)
    * chore: Add favorite drink functionality to user resource
    
    * feat: Add @ApiTags('Users') to UsersController
    
    * pr fixes

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -29,5 +31,21 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(Number(id));
+  }
+
+  @Post(':id/favoriteDrinks/:favoriteDrinkId')
+  addFavoriteDrink(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('favoriteDrinkId', new ParseUUIDPipe()) favoriteDrinkId: string
+  ) {
+    return this.usersService.addFavoriteDrink(id, favoriteDrinkId);
+  }
+
+  @Delete(':id/favoriteDrinks/:favoriteDrinkId')
+  removeFavoriteDrink(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('favoriteDrinkId', new ParseUUIDPipe()) favoriteDrinkId: string
+  ) {
+    return this.usersService.removeFavoriteDrink(id, favoriteDrinkId);
   }
 }
--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class UsersService {
+  constructor(readonly prisma: PrismaService) {}
+
   create(/*createUserDto: CreateUserDto*/) {
     return 'This action adds a new user';
   }
@@ -20,5 +24,56 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async addFavoriteDrink(userId: string, favoriteDrinkId: string): Promise<User> {
+    const [user, drink] = await Promise.all([
+      this.prisma.user.findUnique({
+        where: { authSchId: userId },
+        include: { favouriteDrinks: true },
+      }),
+      this.prisma.drink.findUnique({ where: { id: favoriteDrinkId } }),
+    ]);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (!drink) {
+      throw new NotFoundException('Drink not found');
+    }
+
+    if (user.favouriteDrinks.length >= 3) {
+      throw new BadRequestException('A user can have only 3 favorite drinks.');
+    }
+
+    if (user.favouriteDrinks.some((drink) => drink.id === favoriteDrinkId)) {
+      throw new BadRequestException('The drink is already in the user list of favorite drinks.');
+    }
+
+    return this.prisma.user.update({
+      where: { authSchId: userId },
+      data: { favouriteDrinks: { connect: { id: favoriteDrinkId } } },
+    });
+  }
+
+  async removeFavoriteDrink(userId: string, favoriteDrinkId: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { authSchId: userId },
+      include: { favouriteDrinks: { where: { id: favoriteDrinkId } } },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (user.favouriteDrinks.length === 0) {
+      throw new BadRequestException("The drink is not in the user's list of favorite drinks.");
+    }
+
+    return this.prisma.user.update({
+      where: { authSchId: userId },
+      data: { favouriteDrinks: { disconnect: { id: favoriteDrinkId } } },
+    });
   }
 }
