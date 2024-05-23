b31d097b1a9029032dd225ba9a3e47f2512abb6d
Görömbey Lilla <134506897+murtagh27@users.noreply.github.com>
2024-05-23 13:24:44 +0200

    Update apps/backend/src/drink-action/entities/drink-action.entity.ts
    Co-authored-by: Farkasházi Levente <79463263+LeventeFarkashazi@users.noreply.github.com>

--- apps/backend/src/drink-action/entities/drink-action.entity.ts
+++ apps/backend/src/drink-action/entities/drink-action.entity.ts
@@ -1,12 +1,23 @@
 import { IsNumber, IsUUID } from 'class-validator';
 
 export class DrinkAction {
+  /**
+   * @example "123e4567-e89b-12d3-a456-426614174000"
+   */
   @IsUUID()
   id: string;
 
+  /**
+   * Id of the drink involved in the action
+   * @example "123e4567-e89b-12d3-a456-426614174001"
+   */
   @IsUUID()
   drinkId: string;
 
+  /**
+   * Id of the event where the drink action happened
+   * @example "123e4567-e89b-12d3-a456-426614174002"
+   */
   @IsUUID()
   eventId: string;
 
