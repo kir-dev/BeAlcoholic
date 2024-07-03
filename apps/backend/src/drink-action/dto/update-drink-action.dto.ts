4c052c7eefafa0f0f861ae846e10b9312eba236f
Görömbey Lilla <134506897+murtagh27@users.noreply.github.com>
2024-07-03 20:43:30 +0200

    implement 10 and 11 issues (#15)
    * implement 10 and 11 issues
    
    * corrected package.json
    
    * Update apps/backend/src/drink-action/entities/drink-action.entity.ts
    
    Co-authored-by: Farkasházi Levente <79463263+LeventeFarkashazi@users.noreply.github.com>
    
    * extra validation added in drink-action.entity
    
    * i made a new dto and also fixed some other things connected to that hopefully
    
    * error handling corrected
    
    ---------
    
    Co-authored-by: Farkasházi Levente <79463263+LeventeFarkashazi@users.noreply.github.com>
    Co-authored-by: Sámuel Fekete <feketesamu@gmail.com>

---
+++ apps/backend/src/drink-action/dto/update-drink-action.dto.ts
@@ -0,0 +1,5 @@
+import { OmitType, PartialType } from '@nestjs/swagger';
+
+import { CreateDrinkActionDto } from './create-drink-action.dto';
+
+export class UpdateDrinkActionDto extends OmitType(PartialType(CreateDrinkActionDto), ['drinkId', 'eventId']) {}
