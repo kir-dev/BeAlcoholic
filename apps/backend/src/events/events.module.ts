97a59dd95caa5ea83799d9753628a899c7f17e41
Várallyay_Máté <mate.varallyay@edu.bme.hu>
2024-08-13 11:20:06 +0200

    Updated the Event FindOne function

--- apps/backend/src/events/events.module.ts
+++ apps/backend/src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { EventWithDrinkActionsAndUser } from './dto/event-with-drinkActions-and-user.dto';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
+  imports: [EventWithDrinkActionsAndUser],
   controllers: [EventsController],
   providers: [EventsService],
 })
